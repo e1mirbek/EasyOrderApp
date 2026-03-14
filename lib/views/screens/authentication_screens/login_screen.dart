1cb2fb65b19bb9d57688ac9ab3c91a8442d18ad8
Элмирбек Абдуманапов <elmirbekceo@MacBook-Air-Elmirbek.local>
2026-03-14 18:41:51 +0600

    ferat(auth): add email and password fields to login screen

--- lib/views/screens/authentication_screens/login_screen.dart
+++ lib/views/screens/authentication_screens/login_screen.dart
@@ -1,9 +1,8 @@
-
-
 import 'package:easy_order/core/constants/app_assets.dart';
 import 'package:easy_order/core/constants/app_sizes.dart';
 import 'package:easy_order/core/theme/app_text_styles.dart';
 import 'package:easy_order/generated/l10n.dart';
+import 'package:easy_order/views/widgets/labeled_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -12,37 +11,62 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Padding(
-        padding: const EdgeInsets.all(AppSizes.screenPadding),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _buildHeader(context),
-              const SizedBox(height: AppSizes.spaceSmall,),
-              _buildIllustration(),
-            ],
-          ),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Padding(
+                  padding: const EdgeInsets.all(AppSizes.screenPadding),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildHeader(context),
+                        const SizedBox(height: AppSizes.spaceSmall),
+                        _buildIllustration(),
+                        const SizedBox(height: AppSizes.spaceSmall),
+                        LabeledTextField(
+                          label: S.of(context).emailLabel,
+                          hintText: S.of(context).emailHint,
+                        ),
+                        const SizedBox(height: AppSizes.spaceSmall),
+                        LabeledTextField(
+                          label: S.of(context).passwordLabel,
+                          hintText: S.of(context).passwordHint,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
-      )),
+      ),
     );
   }
 
-  Image _buildIllustration() {
+  Widget _buildIllustration() {
     return Image.asset(
-                    AppAssets.welcomeIllu,
-                    height: AppSizes.welcomeImageSize,
-                    width: AppSizes.welcomeImageSize,
-                  );
+      AppAssets.welcomeIllu,
+      height: AppSizes.welcomeImageSize,
+      width: AppSizes.welcomeImageSize,
+    );
   }
 
   Widget _buildHeader(BuildContext context) {
     return Column(
       children: [
         Text(S.of(context).loginTitle, style: AppTextStyles.title),
-        const SizedBox(height: AppSizes.spaceSmall,),
-          Text(S.of(context).loginSubtitle,  style: AppTextStyles.subtitle, textAlign: TextAlign.center,),
+        const SizedBox(height: AppSizes.spaceSmall),
+        Text(
+          S.of(context).loginSubtitle,
+          style: AppTextStyles.subtitle,
+          textAlign: TextAlign.center,
+        ),
       ],
     );
-  } 
+  }
 }

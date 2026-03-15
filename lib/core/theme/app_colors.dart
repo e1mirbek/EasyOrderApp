a1ff59d8fb23176f2ab5ebc9c8d273d128062724
Элмирбек Абдуманапов <elmirbekceo@MacBook-Air-Elmirbek.local>
2026-03-15 20:34:36 +0600

    chore: add new colors for theme data

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color greyLight = Color(0xFFF5F5F5);
   static const Color primaryText = Color(0xFF0D120E);
   static const Color secondaryText = Color(0xFF6B7280);
   static const Color primary = Color(0xFF1E40AF);
+  static const Color grey = Colors.grey;
+  static const Color fieldBorder = Color(0xFFE5E7EB);
+
+  // 2. Семантические цвета (Functional Colors)
+  // Это помогает понять, ЗАЧЕМ нужен цвет, а не какой он
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color border = Color(0xFFE00E0E);
 }

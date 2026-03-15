12eb805b005af81083f3660ff0a3b63fcbeb1861
Элмирбек Абдуманапов <elmirgrowth@gmail.com>
2026-03-15 21:26:15 +0600

    feat: add custom button widget

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,21 @@
+import 'package:easy_order/core/theme/app_colors.dart';
+import 'package:easy_order/core/theme/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final void Function()? onPressed;
+  const CustomButton({super.key, this.onPressed, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(15.0),
+        foregroundColor: AppColors.white,
+      ),
+      onPressed: onPressed,
+      child: Center(child: Text(title, style: AppTextStyles.buttontitle)),
+    );
+  }
+}

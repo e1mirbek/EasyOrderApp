3e43d94d805a50306d3144aaac835607a96c64d2
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-17 14:23:47 +0600

    chore: add app constants

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -2,4 +2,7 @@ class AppAssets {
   static const String welcomeIllu = 'assets/images/Illustration.png';
   static const String emaiIcon = 'assets/icons/email.png';
   static const String passwordIcon = 'assets/icons/password.png';
+  static const String isFlagEn = 'assets/icons/usa.svg';
+  static const String isFlagRu = 'assets/icons/ru.svg';
+  static const String isFlagKy = 'assets/icons/ky.svg';
 }

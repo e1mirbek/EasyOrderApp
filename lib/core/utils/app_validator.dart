769460fb4af41b64e08b465e7d078cff2c026a7a
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-23 14:49:17 +0600

    feat(core): add validator RegExp and function for validate email, password, name

--- lib/core/utils/app_validator.dart
+++ lib/core/utils/app_validator.dart
@@ -6,6 +6,14 @@ class AppValidator {
     r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
   );
 
+  static final _passwordValidate = RegExp(
+    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
+  );
+
+  static final _nameValidate = RegExp(r"^[a-zA-Zа-яА-ЯёЁ\s'-]+$");
+
+  // email validate
+
   static String? validateEmail(String? value, BuildContext context) {
     if (value == null || value.isEmpty) {
       return S.of(context).emailRequired;
@@ -15,6 +23,37 @@ class AppValidator {
       return S.of(context).emailInvalid;
     }
 
+    return null;
+  }
+
+  // password validate
+
+  static String? validatePassword(String? value, BuildContext context) {
+    if (value == null || value.isEmpty) {
+      return S.of(context).passwordRequired;
+    }
+
+    if (!_passwordValidate.hasMatch(value)) {
+      return S.of(context).passwordTooShort;
+    }
+
+    return null;
+  }
+
+  // email validate
+
+  static String? validateName(String? value, BuildContext context) {
+    if (value == null || value.trim().isEmpty) {
+      return S.of(context).fullNameRequired;
+    }
+
+    if (value.trim().length < 2) {
+      return S.of(context).fullNameTooShort;
+    }
+
+    if (!_nameValidate.hasMatch(value)) {
+      return S.of(context).fullNameTooShort;
+    }
     return null;
   }
 }

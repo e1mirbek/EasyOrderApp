39e4d24b7694f4b721891d193e032d06a80c5861
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-23 19:16:55 +0600

    build: connect firebase to android and update gitignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.easy_order"
+        applicationId = "com.manap.easy_order"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion

85c2b60e60886b939fe7a95058d0516efac05d3e
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-23 13:24:50 +0600

    feat: add app validators

---
+++ lib/core/utils/app_validator.dart
@@ -0,0 +1,11 @@
+class AppValidator {
+
+  // Регулярное выражение для проверки Email
+  static final RegExp _emailRegExp = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+
+  // Валидатор Email
+  
+
+}

e571309a1142f44969a206b4314e58327a443eec
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-23 14:07:23 +0600

    feat(core): add email validate with RegExp

--- lib/core/utils/app_validator.dart
+++ lib/core/utils/app_validator.dart
@@ -1,11 +1,20 @@
-class AppValidator {
+import 'package:easy_order/generated/l10n.dart';
+import 'package:flutter/material.dart';
 
-  // Регулярное выражение для проверки Email
+class AppValidator {
   static final RegExp _emailRegExp = RegExp(
     r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
   );
 
-  // Валидатор Email
-  
+  static String? validateEmail(String? value, BuildContext context) {
+    if (value == null || value.isEmpty) {
+      return S.of(context).emailRequired;
+    }
 
+    if (!_emailRegExp.hasMatch(value)) {
+      return S.of(context).emailInvalid;
+    }
+
+    return null;
+  }
 }
